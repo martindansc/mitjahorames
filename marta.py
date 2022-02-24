bca45065ed4240299559e2bbf00e1d9fca4782cb
und1n3 <undineunda5@gmail.com>
2022-02-24 20:17:50 +0100

    function score_projects (in marta.py)

--- marta.py
+++ marta.py
@@ -17,3 +17,22 @@ def choose_contrib(contributors,skill,level):
     if df.shape[0]>0:
         return df['name'].iloc[0]
     return None
+
+
+def score_projects(dict_projects, list_projects, time):
+    """
+    dict of all projects, list of projects available (names), current
+    """
+    d = {'name': [], 'deadline': [], 'score': [], 'needed_contributors': []}
+
+    for proj in list_projects:
+        d['name'].append(dict_projects[proj].name)
+        d['score'].append(dict_projects[proj].score)
+        d['deadline'].append(eval(dict_projects[proj].deadline))
+        d['needed_contributors'].append(dict_projects[proj].needed_contributors)
+    df = pd.DataFrame(d)
+    df['score_total'] = df.apply(
+        lambda row: row['score'] - (time - row['deadline']) if time > row['deadline'] else row['score'], axis=1)
+    df = df.sort_values(by=['score_total', 'needed_contributors'], ascending=[False, True])
+    return list(df['name'])
+
